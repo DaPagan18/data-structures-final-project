478bed927b7db152cd9c577313c58c1c48a0041a
Calum29d <calumdavies08@gmail.com>
2026-03-18 22:09:36 +0000

    added getters and setters for each field

--- messengerProgram/src/datastructProject/Contact.java
+++ messengerProgram/src/datastructProject/Contact.java
@@ -12,6 +12,54 @@ public class Contact {
     private String name;
     private String phoneNumber;
     private String profilePicPath;
-    private HashMap<String, Contact> contactsByUsername;
+    private HashMap<String, Contact> contactsByNumber;
+
+    public Contact(){
+        name = "";
+        phoneNumber = "";
+        profilePicPath = "";
+        contactsByNumber = new HashMap<>();
+    }
+
+    public Contact(String name, String phoneNumber, String profilePicPath){
+        this.name = name;
+        this.profilePicPath = profilePicPath;
+        this.profilePicPath = profilePicPath;
+        contactsByNumber = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfilePicPath() {
+        return profilePicPath;
+    }
+
+    public void setProfilePicPath(String profilePicPath) {
+        this.profilePicPath = profilePicPath;
+    }
+
+    public Contact getContactsByUsername(String name) {
+        return contactsByNumber.get(name);
+    }
+
+    public void addContact(Contact contact){
+        contactsByNumber.put(contact.getPhoneNumber(), contact);
+    }
+
+    
 
 }
