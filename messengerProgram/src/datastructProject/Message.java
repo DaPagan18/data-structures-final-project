469c4005ebcd29f18ccbf5edff87a00c4f21e1c5
Cal-Sin <255899529+Cal-Sin@users.noreply.github.com>
2026-04-04 14:49:38 +0100

    JavaDoc for Message class

--- messengerProgram/src/datastructProject/Message.java
+++ messengerProgram/src/datastructProject/Message.java
@@ -8,7 +8,8 @@ package datastructProject;
 
 import java.time.LocalDateTime;
 
-public class Message {
+public class Message 
+{
     private String id;
     private String chatId;
     private String from;
@@ -17,7 +18,12 @@ public class Message {
     private boolean read;
     private boolean liked;
 
-    public Message() {
+    // ### CONSTRUCTORS ### //
+    /*
+     * Default constructor for creating a new Message instance
+     */
+    public Message() 
+    {
         this.id = "";
         this.chatId = "";
         this.from = "";
@@ -27,7 +33,17 @@ public class Message {
         this.liked = false;
     }
 
-    public Message(String id, String chatId, String from, String messageContent, LocalDateTime timeSent) {
+    /*
+     * Overloaded constructor for creating a new Message instance with specified values
+     * 
+     * @param id Unique identifier for the message
+     * @param chatId Identifier for the chat this message belongs to   
+     * @param from Sender of the message
+     * @param messageContent Content of the message
+     * @param timeSent Timestamp of when the message was sent
+    */
+    public Message(String id, String chatId, String from, String messageContent, LocalDateTime timeSent) 
+    {
         this.id = id;
         this.chatId = chatId;
         this.from = from;
@@ -37,60 +53,144 @@ public class Message {
         this.liked = false;
     }
 
-    public String getId() {
+    // ### GETTERS AND SETTERS ### //
+    /*
+     * Gets the unique identifier for the message
+     * 
+     * @return String The message ID
+     */
+    public String getId() 
+    {
         return id;
     }
 
-    public void setId(String id) {
+    /*
+     * Sets the unique identifier for the message
+     * 
+     * @param id The message ID
+     */
+    public void setId(String id) 
+    {
         this.id = id;
     }
 
-    public String getChatId() {
+    /*
+     * Gets the identifier for the chat this message belongs to
+     * 
+     * @return String The chat ID
+     */
+    public String getChatId() 
+    {
         return chatId;
     }
 
-    public void setChatId(String chatId) {
+    /*
+     * Sets the identifier for the chat this message belongs to
+     * 
+     * @param chatId The chat ID
+     */
+    public void setChatId(String chatId) 
+    {
         this.chatId = chatId;
     }
 
-    public String getFrom() {
+    /*
+     * Gets the sender of the message
+     * 
+     * @return String The sender's name or identifier
+     */
+    public String getFrom() 
+    {
         return from;
     }
 
-    public void setFrom(String from) {
+    /*
+     * Sets the sender of the message
+     * 
+     * @param from The sender's name or identifier
+     */
+    public void setFrom(String from) 
+    {
         this.from = from;
     }
 
-    public String getMessageContent() {
+    /*
+     * Gets the content of the message
+     * 
+     * @return String The message content
+     */
+    public String getMessageContent()
+    {
         return messageContent;
     }
 
-    public void setMessageContent(String messageContent) {
+    /*
+     * Sets the content of the message
+     * 
+     * @param messageContent The message content
+     */
+    public void setMessageContent(String messageContent) 
+    {
         this.messageContent = messageContent;
     }
 
-    public LocalDateTime getTimeSent() {
+    /*
+     * Gets the timestamp of when the message was sent
+     * 
+     * @return LocalDateTime The time the message was sent
+     */
+    public LocalDateTime getTimeSent() 
+    {
         return timeSent;
     }
 
-    public void setTimeSent(LocalDateTime timeSent) {
+    /*
+     * Sets the timestamp of when the message was sent
+     * 
+     * @param timeSent The time the message was sent
+     */
+    public void setTimeSent(LocalDateTime timeSent) 
+    {
         this.timeSent = timeSent;
     }
 
-    public boolean isRead() {
+    /*
+     * Checks if the message has been read
+     * 
+     * @return boolean True if the message has been read, false otherwise
+     */
+    public boolean isRead() 
+    {
         return read;
     }
 
-    public void setRead(boolean read) {
+    /*
+     * Sets the read status of the message
+     * 
+     * @param read True if the message has been read, false otherwise
+     */
+    public void setRead(boolean read) 
+    {
         this.read = read;
     }
 
-    public boolean isLiked() {
+    /*
+     * Checks if the message has been liked
+     * 
+     * @return boolean True if the message has been liked, false otherwise
+     */
+    public boolean isLiked() 
+    {
         return liked;
     }
 
-    public void setLiked(boolean liked) {
+    /*
+     * Sets the liked status of the message
+     * 
+     * @param liked True if the message has been liked, false otherwise
+     */
+    public void setLiked(boolean liked) 
+    {
         this.liked = liked;
     }
-
 }
