1cc99854adf3b34d7a63f721657e697de0d48a06
Cal-Sin <255899529+Cal-Sin@users.noreply.github.com>
2026-04-04 14:34:57 +0100

    JavaDoc for Profile Page and NavigationManagaer

--- messengerProgram/src/datastructProject/NavigationManager.java
+++ messengerProgram/src/datastructProject/NavigationManager.java
@@ -1,10 +1,16 @@
+/**
+ * NavigationManager class that manages the navigation between different pages in the application
+ *
+ * @author Calum Davies
+ */
 package datastructProject;
 
 import java.awt.CardLayout;
 import java.util.HashMap;
 import javax.swing.JPanel;
 
-public class NavigationManager {
+public class NavigationManager 
+{
     private static NavigationManager instance;
     private CardLayout cardLayout;
     private JPanel cardPanel;
@@ -13,62 +19,136 @@ public class NavigationManager {
     private String currentUserPhone;
     private HashMap<String, JPanel> pages = new HashMap<>();
 
+    // Private constructor to prevent instantiation from outside the class
     private NavigationManager() {}
 
-    public static NavigationManager getInstance() {
-        if (instance == null) {
-            instance = new NavigationManager();
-        }
+    /*
+     * Gets the singleton instance of the NavigationManager
+     * 
+     * @return NavigationManager The instance of the NavigationManager
+     */
+    public static NavigationManager getInstance() 
+    {
+        if (instance == null) 
+            {
+                // Create a new instance of the NavigationManager
+                instance = new NavigationManager();
+            }
         return instance;
     }
 
-    public void register(CardLayout cardLayout, JPanel cardPanel) {
+    /*
+     * Registers the card layout and card panel for navigation
+     * 
+     * @param cardLayout The card layout to register
+     * @param cardPanel The card panel to register
+     */
+    public void register(CardLayout cardLayout, JPanel cardPanel) 
+    {
         this.cardLayout = cardLayout;
         this.cardPanel = cardPanel;
     }
 
-    public void storeChatManager(ChatManager chatManager){
+    /*
+     * Stores the chat manager for use in navigation
+     * 
+     * @param chatManager The chat manager to store
+     */
+    public void storeChatManager(ChatManager chatManager)
+    {
         this.chatManager = chatManager;
     }
 
-    public ChatManager getChatManager(){
+    /*
+     * Gets the stored chat manager
+     * 
+     * @return ChatManager The stored chat manager
+     */
+    public ChatManager getChatManager()
+    {
         return chatManager;
     }
 
-    public void storeUserRegistry(UserRegistry userRegistry, String currentUserPhone){
+    /*
+     * Stores the user registry and current user phone number for use in navigation
+     * 
+     * @param userRegistry The user registry to store
+     * @param currentUserPhone The phone number of the current user
+     */
+    public void storeUserRegistry(UserRegistry userRegistry, String currentUserPhone)
+    {
         this.userRegistry = userRegistry;
         this.currentUserPhone = currentUserPhone;
     }
 
-    public UserRegistry getUserRegistry(){
+    /*
+     * Gets the stored user registry
+     * 
+     * @return UserRegistry The stored user registry
+     */
+    public UserRegistry getUserRegistry()
+    {
         return userRegistry;
     }
 
-    public String getCurrentUserPhone(){
+    /*
+     * Gets the phone number of the current user
+     * 
+     * @return String The phone number of the current user
+     */
+    public String getCurrentUserPhone()
+    {
         return currentUserPhone;
     }
 
-    public void navigateTo(String pageName) {
-        if (pages.containsKey(pageName) && pages.get(pageName) instanceof ChatPage) {
-            String id = pageName.substring(5);
-            Chat chat = chatManager.getChat(id);
-            if (chat != null) {
-                ((ChatPage) pages.get(pageName)).updateChat(chat);
+    /*
+     * Navigates to the specified page
+     * 
+     * @param pageName The name of the page to navigate to
+     */
+    public void navigateTo(String pageName) 
+    {
+        // If statement to check if the page is a chat page
+        if (pages.containsKey(pageName) && pages.get(pageName) instanceof ChatPage) 
+            {
+                String id = pageName.substring(5);
+                Chat chat = chatManager.getChat(id);
+                if (chat != null)
+                    {
+                        // Update the chat page with the latest chat data
+                        ((ChatPage) pages.get(pageName)).updateChat(chat);
+                    }
             }
-        }
-        if (pageName.equals("Edit Profile") && pages.containsKey(pageName)) {
-            ((EditProfilePage) pages.get(pageName)).refresh();
-        }
-        if (pageName.equals("Home") && pages.containsKey(pageName)) {
-            ((HomePage) pages.get(pageName)).refresh();
-        }
-        if (pageName.equals("Contacts") && pages.containsKey(pageName)) {
-            ((ContactsPage) pages.get(pageName)).refresh();
-        }
+            // If statement to check if the page is the edit profile page
+            if (pageName.equals("Edit Profile") && pages.containsKey(pageName)) 
+                {
+                    // Refresh the edit profile page to show the latest profile data
+                    ((EditProfilePage) pages.get(pageName)).refresh();
+                }
+                // If statement to check if the page is the home page 
+                if (pageName.equals("Home") && pages.containsKey(pageName)) 
+                    {
+                        // Refresh the home page to show the latest profile and contact data
+                        ((HomePage) pages.get(pageName)).refresh();
+                    }
+                    // If statement to check if the page is the contacts page
+                    if (pageName.equals("Contacts") && pages.containsKey(pageName)) 
+                    {
+                        // Refresh the contacts page to show the latest contact data
+                        ((ContactsPage) pages.get(pageName)).refresh();
+                    }
+        // Show the page using the card layout
         cardLayout.show(cardPanel, pageName);
     }
 
-    public void registerPage(JPanel page, String name){
+    /*
+     * Registers a page with the navigation manager
+     * 
+     * @param page The page to register
+     * @param name The name of the page
+     */
+    public void registerPage(JPanel page, String name)
+    {
         cardPanel.add(page, name);
         pages.put(name, page);
     }
--- messengerProgram/src/datastructProject/Profile.java
+++ messengerProgram/src/datastructProject/Profile.java
@@ -8,25 +8,26 @@ package datastructProject;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class Profile implements Iterable<Contact> {
-
+public class Profile implements Iterable<Contact> 
+{
     private String name;
     private String phoneNumber;
     private String profilePicPath;
     private final HashMap<String, Contact> contactsByNumber = new HashMap<>();
 
     //creates an empty profile ready to be edited
-    public Profile(UserRegistry userRegistry) {
+    public Profile(UserRegistry userRegistry) 
+    {
         this.name = "";
         this.phoneNumber = generateUniquePhoneNumber(userRegistry);
         this.profilePicPath = "";
         userRegistry.addProfile(this);
-
     }
 
   
     //this contsructor is currently used to create test profiles
-    public Profile(String name, String profilePicPath, UserRegistry userRegistry){
+    public Profile(String name, String profilePicPath, UserRegistry userRegistry)
+    {
         this.name = name;
         this.profilePicPath = profilePicPath;
         this.phoneNumber = generateUniquePhoneNumber(userRegistry);
@@ -44,84 +45,171 @@ public class Profile implements Iterable<Contact> {
         userRegistry.addProfile(this);
     }
 
-
-
-    //getters and setters for profile data
-    public String getName() {
+    // ### GETTERS AND SETTERS ###
+    /*
+     * Gets the name of the profile
+     * 
+     * @return String The name of the profile
+     */
+    public String getName() 
+    {
         return name;
     }
 
-    public void setName(String name) {
+    /*
+     * Sets the name of the profile
+     * 
+     * @param name The name to set
+     */
+    public void setName(String name) 
+    {
         this.name = name;
     }
 
-    public String getPhoneNumber() {
+    /*
+     * Gets the phone number of the profile
+     * 
+     * @return String The phone number of the profile
+     */
+    public String getPhoneNumber() 
+    {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
+    /*
+     * Sets the phone number of the profile
+     * 
+     * @param phoneNumber The phone number to set
+     */
+    public void setPhoneNumber(String phoneNumber) 
+    {
         this.phoneNumber = phoneNumber;
     }
 
+    /*
+     * Gets the profile picture path of the profile
+     * 
+     * @return String The profile picture path of the profile
+     */
     public String getProfilePicPath() {
         return profilePicPath;
     }
 
-    public void setProfilePicPath(String profilePicPath) {
+    /*
+     * Sets the profile picture path of the profile
+     * 
+     * @param profilePicPath The profile picture path to set
+     */
+    public void setProfilePicPath(String profilePicPath) 
+    {
         this.profilePicPath = profilePicPath;
     }
 
-    //getters and setters for the hashmap of contacts
-    public void addContact(Contact contact) {
+    /*
+     * Adds a contact to the profile
+     * 
+     * @param contact The contact to add
+     */
+    public void addContact(Contact contact) 
+    {
         contactsByNumber.put(contact.getPhoneNumber(), contact);
     }
 
-    public Contact getContact(String phoneNumber) {
+    /*
+     * Gets a contact by phone number
+     * 
+     * @param phoneNumber The phone number of the contact to get
+     * @return Contact The contact if found, null otherwise
+     */
+    public Contact getContact(String phoneNumber) 
+    {
         return contactsByNumber.get(phoneNumber);
     }
 
-    public boolean removeContact(String phoneNumber) {
+    /*
+     * Removes a contact from the profile
+     * 
+     * @param phoneNumber The phone number of the contact to remove
+     * @return boolean True if the contact was removed, false otherwise
+     */
+    public boolean removeContact(String phoneNumber) 
+    {
+        // Remove the contact from the profile's contact list
         boolean removed = contactsByNumber.remove(phoneNumber) != null;
         
-        if (removed) {
-            ChatManager chatManager = NavigationManager.getInstance().getChatManager();
-            String chatId;
-            if (this.phoneNumber.compareTo(phoneNumber) <= 0) {
-                chatId = this.phoneNumber + "_" + phoneNumber;
-            } else {
-                chatId = phoneNumber + "_" + this.phoneNumber;
-            }
+        // If statement to check if the contact was successfully removed
+        if (removed) 
+            {
+                // Get the chat manager instance
+                ChatManager chatManager = NavigationManager.getInstance().getChatManager();
+                String chatId;
+                if (this.phoneNumber.compareTo(phoneNumber) <= 0) 
+                    {
+                    chatId = this.phoneNumber + "_" + phoneNumber;
+                    } else {
+                        chatId = phoneNumber + "_" + this.phoneNumber;
+                    }
+            // Remove the chats associated with the contact
             chatManager.deleteChat(chatId);
-        }
+            }
         return removed;
     }
 
-    public boolean hasContact(String phoneNumber) {
+    /*
+     * Checks if the profile has a contact with the given phone number
+     * 
+     * @param phoneNumber The phone number of the contact to check
+     * @return boolean True if the contact exists, false otherwise
+     */
+    public boolean hasContact(String phoneNumber) 
+    {
         return contactsByNumber.containsKey(phoneNumber);
     }
 
-    public HashMap<String, Contact> getAllContacts() {
+    /*
+     * Gets all contacts of the profile
+     * 
+     * @return HashMap<String, Contact> The map of all contacts
+     */
+    public HashMap<String, Contact> getAllContacts() 
+    {
         return contactsByNumber;
     }
 
-    private String generateUniquePhoneNumber(UserRegistry userRegistry){
-
+    /*
+     * Generates a unique phone number for the profile
+     * 
+     * @param userRegistry The user registry to check for existing phone numbers
+     * @return String The unique phone number
+     */
+    private String generateUniquePhoneNumber(UserRegistry userRegistry)
+    {
+        // Declare a variable to store the generated phone number
         String phoneNumber;
 
+        //Do-while loop to generate a unique phone number that is not already in the user registry
         do {
+            // Generate a random 10-digit phone number
             StringBuilder randPhoneNum = new StringBuilder(10);
-            for (int i = 0; i < 10; i++) {
-                int randNum = (int) (Math.random() * 10);
-                randPhoneNum.append(randNum);
-            }
+            for (int i = 0; i < 10; i++) 
+                {
+                    int randNum = (int) (Math.random() * 10);
+                    randPhoneNum.append(randNum);
+                }
             phoneNumber = randPhoneNum.toString();
         }while(userRegistry.lookup(phoneNumber));
 
         return phoneNumber;
     }
 
+    /*
+     * Returns an iterator over the contacts in the profile
+     * 
+     * @return Iterator<Contact> The iterator over the contacts
+     */
     @Override
-    public Iterator<Contact> iterator() {
+    public Iterator<Contact> iterator() 
+    {
         return contactsByNumber.values().iterator();
     }
 
