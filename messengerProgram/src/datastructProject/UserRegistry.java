0968fab712032ec2c18b546190a38e3cefc7e521
Cal-Sin <255899529+Cal-Sin@users.noreply.github.com>
2026-04-04 13:23:42 +0100

    Javadoc for UserRegistry class

--- messengerProgram/src/datastructProject/UserRegistry.java
+++ messengerProgram/src/datastructProject/UserRegistry.java
@@ -8,19 +8,40 @@ package datastructProject;
 
 import java.util.HashMap;
 
-public class UserRegistry {
-
+public class UserRegistry 
+{
+    // HashMap to store user profiles, with the phone number as the key for quick lookup
     private final HashMap<String, Profile> profilesByPhoneNumber = new HashMap<>();
 
-    public void addProfile(Profile profile){
+    /*
+     * Adds a profile to the user registry
+     * 
+     * @param profile The profile to be added
+     */
+    public void addProfile(Profile profile)
+    {
         profilesByPhoneNumber.put(profile.getPhoneNumber(), profile);
     }
 
-    public boolean lookup(String phoneNumber){
+    /*
+     * Looks up a profile by phone number
+     * 
+     * @param phoneNumber The phone number of the profile to look up
+     * @return boolean True if the profile exists, false otherwise
+     */
+    public boolean lookup(String phoneNumber)
+    {
         return profilesByPhoneNumber.containsKey(phoneNumber);
     }
 
-    public Profile getProfile(String phoneNumber){
+    /*
+     * Gets a profile by phone number
+     * 
+     * @param phoneNumber The phone number of the profile to get
+     * @return Profile The profile if found, null otherwise
+     */
+    public Profile getProfile(String phoneNumber)
+    {
         return profilesByPhoneNumber.get(phoneNumber);
     }
 
